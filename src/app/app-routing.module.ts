e5a227a14e9a3c09ce19f35d36f2fc8b4e78cf9d
Mohamed-78 <dieudonneirie7@gmail.com>
2021-06-29 14:14:14 +0000

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -11,6 +11,10 @@ const routes: Routes = [
     redirectTo: 'home',
     pathMatch: 'full'
   },
+  {
+    path: 'register',
+    loadChildren: () => import('./register/register.module').then( m => m.RegisterPageModule)
+  },
 ];
 
 @NgModule({
